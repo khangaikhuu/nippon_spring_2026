2a65cd97f3bf2edfc28440d8bceffab78d7d2453
Uvgunkhuu Khangaikhuu <khangaikhuu@hey.com>
2026-06-29 18:00:51 +0800

    update binary search test

--- month04/week14/day01/week14-day01-backend/src/test/java/mn/icode/BinarySearchTest.java
+++ month04/week14/day01/week14-day01-backend/src/test/java/mn/icode/BinarySearchTest.java
@@ -10,6 +10,23 @@ public class BinarySearchTest {
 		BinarySearch binarySearch = new BinarySearch(sortedList);
 		
 		Assertions.assertEquals(4, binarySearch.binarySearch(9));
-		Assertions.assertEquals(-1, binarySearch.binarySearch(16));
+		Assertions.assertEquals(7, binarySearch.binarySearch(15));
+		Assertions.assertEquals(-1, binarySearch.binarySearch(0));
+		// step 1, target = 16
+		// left =0, right = 7
+		// left <= right => true
+		// mid = 0 + (7 - 0)/2 = 4
+		// numbers[4]= 9 
+		// 9 < 16
+		// left = 4 + 1 = 5, right = 7, mid = 4
+		// 4 < 7
+		// mid = 4 + (7 - 4)/2 = 5
+		// numbers[5] = 11
+		// 11 < 16
+		// left = 5 + 1 = 6, right = 7, mid = 5
+		// mid = 6 + (7 - 6)/ 2 = 6
+		// 13 < 16
+		// mid = 6 + (7 - 6)/2 = 7
+		// 15 == 15 => 7
 	}
 }
